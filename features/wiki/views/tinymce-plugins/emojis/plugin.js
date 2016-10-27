a1430dd5d6649be1a4a8346b856d755e31420a89
Xavier Boubert <xavier@boubert.fr>
2016-10-27 14:28:13 +0200

    fix(emojis plugin): move the _hasEmojis() function to not be mangled by uglifyjs
    fixes #7

--- features/wiki/views/tinymce-plugins/emojis/plugin.js
+++ features/wiki/views/tinymce-plugins/emojis/plugin.js
@@ -68,78 +68,77 @@
       return true;
     }
 
-    editor.addContextToolbar(
-      function hasEmojis(element) {
-        if (!element || !editor.getBody().contains(element)) {
-          _lastEmojiId = null;
-
-          return false;
-        }
-
-        var $element = $(element),
-            emojiId = $element.attr('data-emoji');
-
-        if (!emojiId) {
-          _lastEmojiId = null;
-
-          return false;
-        }
-
-        if (emojiId == _lastEmojiId) {
-          return true;
-        }
-
-        _lastEmojiId = emojiId;
-
-        var post = WikiService.lastReadPost();
-
-        if (!post || !post.emojis || !post.emojis.nodes) {
-          return false;
-        }
-
-        for (var i = 0; i < post.emojis.nodes.length; i++) {
-          var node = post.emojis.nodes[i];
-
-          if (node.id == emojiId) {
-            if (editor.contextToolbars && editor.contextToolbars.length) {
-              for (var j = 0; j < editor.contextToolbars.length; j++) {
-                var toolbar = editor.contextToolbars[j];
-
-                if (!toolbar || !toolbar.predicate || typeof toolbar.predicate != 'function') {
-                  return;
-                }
-
-                var predicateFunction = toolbar.predicate.toString().substr('function '.length);
-                predicateFunction = predicateFunction.substr(0, predicateFunction.indexOf('('));
-
-                if (predicateFunction == 'hasEmojis') {
-                  _toolbar = toolbar;
-                  _node = node;
-
-                  if (toolbar.panel) {
-                    if (!_fillEmojis(false)) {
-                      return false;
-                    }
-                  }
-                  else {
-                    _lastEmojiId = null;
-
-                    setTimeout(_fillEmojis);
-                  }
-
-                  break;
-                }
-              }
-            }
-
-            return true;
-          }
-        }
+    function _hasEmojis(element) {
+      if (!element || !editor.getBody().contains(element)) {
+        _lastEmojiId = null;
 
         return false;
-      },
-      'image'
-    );
+      }
+
+      var $element = $(element),
+          emojiId = $element.attr('data-emoji');
+
+      if (!emojiId) {
+        _lastEmojiId = null;
+
+        return false;
+      }
+
+      if (emojiId == _lastEmojiId) {
+        return true;
+      }
+
+      _lastEmojiId = emojiId;
+
+      var post = WikiService.lastReadPost();
+
+      if (!post || !post.emojis || !post.emojis.nodes) {
+        return false;
+      }
+
+      for (var i = 0; i < post.emojis.nodes.length; i++) {
+        var node = post.emojis.nodes[i];
+
+        if (node.id == emojiId) {
+          if (editor.contextToolbars && editor.contextToolbars.length) {
+            for (var j = 0; j < editor.contextToolbars.length; j++) {
+              var toolbar = editor.contextToolbars[j];
+
+              if (!toolbar || !toolbar.predicate || typeof toolbar.predicate != 'function') {
+                return;
+              }
+
+              var predicateFunction = toolbar.predicate.toString().substr('function '.length);
+              predicateFunction = predicateFunction.substr(0, predicateFunction.indexOf('('));
+
+              if (predicateFunction == '_hasEmojis') {
+                _toolbar = toolbar;
+                _node = node;
+
+                if (toolbar.panel) {
+                  if (!_fillEmojis(false)) {
+                    return false;
+                  }
+                }
+                else {
+                  _lastEmojiId = null;
+
+                  setTimeout(_fillEmojis);
+                }
+
+                break;
+              }
+            }
+          }
+
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    editor.addContextToolbar(_hasEmojis, 'image');
 
     editor.on('init', function() {
       document.getElementsByTagName('head')[0].appendChild(editor.dom.create('link', {
